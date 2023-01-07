dd4c7a187931be371e326e1907c80e6564079826
Tahera29 <120545807+Tahera29@users.noreply.github.com>
2023-01-07 21:46:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,12 +16,30 @@ function PrintDeveloperbyForEach() {
 
 function addData() {
   //Write your code here
+  var arr = [
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ];
+  arr.push({id: 4, name: "susan", age: "20", profession: "intern"});
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here
+  var arr = [
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ];
+  var popped= arr.pop( { id: 3, name: "karen", age: "19", profession: "admin" });
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here
+  var p1=["1","john","18", "developer"];
+  var p2=["2","jack","20","developer"];
+  var p3=["3","karen","19","admin"];
+  console.log(p1.concat(p2,p3));
 }
